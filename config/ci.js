e02a63bc020ef5e0392d21213a391f0364310c1e
Dowan Kim <forty4@gmail.com>
2019-11-13 00:42:23 +0900

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -3,7 +3,7 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  mongoURI: 'mongodb://localhost:27017/blog_everyone',
+  mongoURI: 'mongodb://localhost:27017/blog_ci',
   redisUrl: 'redis://127.0.0.1:6379',
   cookieKey: '123123123'
 };
